43f066fab1fa67bd420ff202c5f00b852f992036
HVIRGEN001 <hvirgen001@vplincsci004.ad.csbsju.edu>
2019-02-26 17:10:11 -0600

    this is so I can initialize my pushes

--- src/cmc/functionality/StudentFunctionalityController.java
+++ src/cmc/functionality/StudentFunctionalityController.java
@@ -4,6 +4,7 @@ public class StudentFunctionalityController {
 
 	public StudentFunctionalityController() {
 		// TODO Auto-generated constructor stub
+		//Just so i can initialize my pushes
 	}
 
 }
