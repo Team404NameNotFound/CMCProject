f86f6fce865f5eadab9725dd0fdc285b5d54cf3f
KMENDEL001 <kmendel001@vplincsci015.ad.csbsju.edu>
2019-03-12 21:53:15 -0500

    Added javadoc comments to entity classes

--- src/cmc/entity/Student.java
+++ src/cmc/entity/Student.java
@@ -3,6 +3,7 @@
  */
 package cmc.entity;
 import java.util.*;
+
 /**
  * @author kmendel001
  *
@@ -12,8 +13,17 @@ public class Student extends Account {
 	/**
 	 * 
 	 */
-	protected ArrayList<UserSavedSchool> savedSchools;
+	private ArrayList<UserSavedSchool> savedSchools;
 
+	/**
+	 * @param firstName first name of the Account
+	 * @param lasName last name of the Account
+	 * @param username username of the Account
+	 * @param password password of the Account
+	 * @param userType type of account admin or student
+	 * @param userStatus whether account is active or inactive
+	 * @param savedSchools schools in the student's saved school list
+	 */
 	public Student(String firstName, String lastName, String username, String password, String userType,
 			String userStatus, ArrayList<UserSavedSchool> savedSchools) {
 		super(firstName, lastName, username, password, userType, userStatus);
@@ -25,14 +35,24 @@ public class Student extends Account {
 		this.savedSchools = savedSchools;
 	}
 
+	/**
+	 * @return list of schools that the student has saved
+	 */
 	public ArrayList<UserSavedSchool> getSavedSchools() {
 		return savedSchools;
 	}
 
+	/**
+	 * @param savedSchools list of schools that the student has saved
+	 */
 	public void setSavedSchools(ArrayList<UserSavedSchool> savedSchools) {
 		this.savedSchools = savedSchools;
 	}
 	
+	/**
+	 * @param universityName name of the university
+	 * @return if the student has saved that particular university
+	 */
 	public boolean isSchoolSaved(String universityName) {
 		boolean found = false;
 		for(UserSavedSchool university: savedSchools) {
@@ -43,6 +63,9 @@ public class Student extends Account {
 		return found;
 	}
 	
+	/**
+	 * @param universityName name of a university
+	 */
 	public void removeSchool(String universityName) {
 		for(int i=0; i<savedSchools.size(); i++) {
 			if(savedSchools.get(i).getName().equals(universityName)) {
@@ -51,6 +74,10 @@ public class Student extends Account {
 		}
 	}
 	
+	/**
+	 * @param university university object
+	 * @param dateAdded date the school was saved
+	 */
 	public void saveSchool(University university, String dateAdded) {
 		UserSavedSchool savedUniversity = new UserSavedSchool(university.getName(), university.getState(), university.getLocation(), 
 				university.getControl(), university.getEnrollment(), university.getPercentFemale(), university.getSatVerbal(), 
--- src/cmc/entity/University.java
+++ src/cmc/entity/University.java
@@ -12,27 +12,47 @@ public class University {
 	/**
 	 * 
 	 */
-	protected String name;
-	protected String state;
-	protected String location;
-	protected String control;
-	protected String enrollment;
-	protected String percentFemale;
-	protected String satVerbal;
-	protected String satMath;
-	protected String cost;
-	protected String percentFinAid;
-	protected String percentEnrolled;
-	protected String applicants;
-	protected String percentAdmitted;
-	protected String academicScale;
-	protected String socialScale;
-	protected String qualityOfLife;
-	protected String[] emphases;
-	protected ArrayList<Student> savedSchool;
+	private String name;
+	private String state;
+	private String location;
+	private String control;
+	private String enrollment;
+	private String percentFemale;
+	private String satVerbal;
+	private String satMath;
+	private String cost;
+	private String percentFinAid;
+	private String percentEnrolled;
+	private String applicants;
+	private String percentAdmitted;
+	private String academicScale;
+	private String socialScale;
+	private String qualityOfLife;
+	private String[] emphases;
+	private ArrayList<Student> savedSchool;
 
 
 
+	/**
+	 * @param name name of the university
+	 * @param state state where the university is located
+	 * @param location setting where the university is located
+	 * @param control what type of control the university is under
+	 * @param enrollment number of students enrolled at the university
+	 * @param percentFemale percentage of the study body that is female
+	 * @param satVerbal average verbal SAT score of student body
+	 * @param satMath average math SAT score of student body
+	 * @param cost annual tuition to attend university
+	 * @param percentFinAid percentage of study body that received financial aid
+	 * @param percentEnrolled percent of applicants that decide to enroll
+	 * @param applicants annual number of applicants
+	 * @param percentAdmitted percent of applicants that get admitted
+	 * @param academicScale integer value between 1 and 5 indicating academic scale of the university
+	 * @param socialScale integer value between 1 and 5 indicating social life at the university
+	 * @param qualityOfLife integer value between 1 and 5 indicating quality of life at the university
+	 * @param emphases up to five areas of study that the university excels in
+	 * @param savedSchool list of students that have saved this school
+	 */
 	public University(String name, String state, String location, String control, String enrollment, String percentFemale,
 			String satVerbal, String satMath, String cost, String percentFinAid, String percentEnrolled, String applicants,
 			String percentAdmitted, String academicScale, String socialScale, String qualityOfLife, String[] emphases,
@@ -58,146 +78,255 @@ public class University {
 		this.savedSchool = savedSchool;
 	}
 
+	/**
+	 * @return university name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * 
+	 * @param name university name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return state where the university is located
+	 */
 	public String getState() {
 		return state;
 	}
 
+	/**
+	 * @param state where the university is located
+	 */
 	public void setState(String state) {
 		this.state = state;
 	}
 
+	/**
+	 * @return what setting the university is located in
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * @param location what setting the university is located in
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	/**
+	 * @return the type of control a university is under
+	 */
 	public String getControl() {
 		return control;
 	}
 
+	/**
+	 * @param control the type of control a university is under
+	 */
 	public void setControl(String control) {
 		this.control = control;
 	}
 
+	/**
+	 * @return the number of students that attend a university
+	 */
 	public String getEnrollment() {
 		return enrollment;
 	}
 
+	/**
+	 * @param enrollment the number of students that attend a university
+	 */
 	public void setEnrollment(String enrollment) {
 		this.enrollment = enrollment;
 	}
 
+	/**
+	 * @return percentage of the student body at the university that is female
+	 */
 	public String getPercentFemale() {
 		return percentFemale;
 	}
 
+	/**
+	 * @param percentFemale percentage of the student body at the university that is female
+	 */
 	public void setPercentFemale(String percentFemale) {
 		this.percentFemale = percentFemale;
 	}
 
+	/**
+	 * @return average verbal SAT score of the student body
+	 */
 	public String getSatVerbal() {
 		return satVerbal;
 	}
 
+	/**
+	 * @param satVerbal average verbal SAT score of the student body
+	 */
 	public void setSatVerbal(String satVerbal) {
 		this.satVerbal = satVerbal;
 	}
 
+	/**
+	 * @return average math SAT score of the student body
+	 */
 	public String getSatMath() {
 		return satMath;
 	}
 
+	/**
+	 * @param satMath average math SAT score of the student body
+	 */
 	public void setSatMath(String satMath) {
 		this.satMath = satMath;
 	}
 
+	/**
+	 * @return annual tuition of a university
+	 */
 	public String getCost() {
 		return cost;
 	}
 
+	/**
+	 * @param cost annual tuition of a university
+	 */
 	public void setCost(String cost) {
 		this.cost = cost;
 	}
 
+	/**
+	 * @return percentage of the student body that received financial aid
+	 */
 	public String getPercentFinAid() {
 		return percentFinAid;
 	}
 
+	/**
+	 * @param percentFinAid percentage of the student body that received financial aid
+	 */
 	public void setPercentFinAid(String percentFinAid) {
 		this.percentFinAid = percentFinAid;
 	}
 
+	/**
+	 * @return percentage of applicants that enroll
+	 */
 	public String getPercentEnrolled() {
 		return percentEnrolled;
 	}
 
+	/**
+	 * @param percentEnrolled percentage of applicants that enroll
+	 */
 	public void setPercentEnrolled(String percentEnrolled) {
 		this.percentEnrolled = percentEnrolled;
 	}
 
+	/**
+	 * @return percentage of applicants that enroll
+	 */
 	public String getApplicants() {
 		return applicants;
 	}
 
+	/**
+	 * @param applicants percentage of applicants that enroll
+	 */
 	public void setApplicants(String applicants) {
 		this.applicants = applicants;
 	}
 
+	/**
+	 * @return percentage of total applicants that are admitted
+	 */
 	public String getPercentAdmitted() {
 		return percentAdmitted;
 	}
 
+	/**
+	 * @param percentAdmitted percentage of total applicants that are admitted
+	 */
 	public void setPercentAdmitted(String percentAdmitted) {
 		this.percentAdmitted = percentAdmitted;
 	}
 
+	/**
+	 * @return academic scale of school
+	 */
 	public String getAcademicScale() {
 		return academicScale;
 	}
 
+	/**
+	 * @param academicScale academica scale of school
+	 */
 	public void setAcademicScale(String academicScale) {
 		this.academicScale = academicScale;
 	}
 
+	/**
+	 * @return social scale of school
+	 */
 	public String getSocialScale() {
 		return socialScale;
 	}
 
+	/**
+	 * @param socialScale social scale of school
+	 */
 	public void setSocialScale(String socialScale) {
 		this.socialScale = socialScale;
 	}
 
+	/**
+	 * @return quality of life scale at school
+	 */
 	public String getQualityOfLife() {
 		return qualityOfLife;
 	}
 
+	/**
+	 * @param qualityOfLife quality of life scale at school
+	 */
 	public void setQualityOfLife(String qualityOfLife) {
 		this.qualityOfLife = qualityOfLife;
 	}
 
+	/**
+	 * @return list of up to five areas that the university excels in
+	 */
 	public String[] getEmphases() {
 		return emphases;
 	}
 
+	/**
+	 * @param emphases list of up to five areas that the university excels in
+	 */
 	public void setEmphases(String[] emphases) {
 		this.emphases = emphases;
 	}
 
+	/**
+	 * @return list of students that saved the school
+	 */
 	public ArrayList<Student> getSavedSchool() {
 		return savedSchool;
 	}
 
+	/**
+	 * @param savedSchool list of students that saved the school
+	 */
 	public void setSavedSchool(ArrayList<Student> savedSchool) {
 		this.savedSchool = savedSchool;
 	}
