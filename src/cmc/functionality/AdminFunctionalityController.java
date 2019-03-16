beb070d994b905ff9ea8072579a8ba06acc92d17
AJHEROUX <ajheroux@vplincsci009.ad.csbsju.edu>
2019-03-16 14:20:46 -0500

    addUniversity and removeUniversity: Untested

--- src/cmc/functionality/AdminFunctionalityController.java
+++ src/cmc/functionality/AdminFunctionalityController.java
@@ -1,5 +1,5 @@
 package cmc.functionality;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import cmc.entity.*;
 /**
  * 
@@ -27,6 +27,23 @@ public class AdminFunctionalityController extends UserFunctionalityController {
 	 */
 	public ArrayList<Account> viewUsers() {
 		return this.DBCon.getAccountList();
+	}
+
+	public void addNewUniversity(String name, String state, String location, String control, String enrollment,
+			String percentFemale, String satVerbal, String satMath, String cost, String percentFinAid,
+			String percentEnrolled, String applicants, String percentAdmitted, String academicScale, String socialScale,
+			String qualityOfLife, String[] emphases) {
+			if (this.DBCon.getUniversity(name) != null) {
+				System.out.println(name + " already exists");
+			}
+			else {
+			University schoolToAdd = this.universityCon.createNewUniversity(name, state, location, control, enrollment, percentFemale, 
+					satVerbal, satMath, cost, percentFinAid, percentEnrolled, applicants, 
+					percentAdmitted, academicScale, socialScale, qualityOfLife, emphases);
+					
+			this.DBCon.addUniversity(schoolToAdd);
+			}
+		
 	}
 
 }
