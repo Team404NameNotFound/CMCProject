015af7489e1b2c985f3530561e8e2a816f8913b5
HVIRGEN001 <hvirgen001@vplincsci086.ad.csbsju.edu>
2019-03-17 11:23:24 -0500

    testing driver on search

--- src/DoyleHerouxVirgenTestDriver.java
+++ src/DoyleHerouxVirgenTestDriver.java
@@ -29,6 +29,19 @@ public class DoyleHerouxVirgenTestDriver {
 		String userPassword = "user";
 		user.login(userUsername, userPassword);
 
-
+		System.out.println("\nSearching and Matching on Fields:");
+		System.out.println("-------------------------");
+		String state = "Minnesota";
+		int numStudentsLower = 10000;
+		int numStudentsUpper = 12000;
+		String[] emphases = {};
+		student.fieldSearch( "", state, "", numStudentsLower,
+				numStudentsUpper, (float)-1.0,(float)-1.0, -1, 
+				-1, -1, -1, -1, -1, 
+				(float)-1.0, (float)-1.0, -1, 
+				-1, (float)-1.0, (float)-1.0,
+				(float)-1.0, (float)-1.0, -1, -1,
+				-1, -1, -1, -1, emphases
+				);
 	}
 }
