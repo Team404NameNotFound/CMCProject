0eec43d2d84431488eea5f7b9fac970cbc60f15d
KMENDEL001 <kmendel001@vplincsci051.ad.csbsju.edu>
2019-04-06 22:36:42 -0500

    Add test for getQuizQuestions

--- tests/cmc/functionality/SearchControllerTest.java
+++ tests/cmc/functionality/SearchControllerTest.java
@@ -2,13 +2,21 @@ package cmc.functionality;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class SearchControllerTest {
+import cmc.entity.University;
 
+public class SearchControllerTest {
+	private DBController dbCon = new DBController();
+	private ArrayList<University> allSchools = dbCon.getUniversityList();
+	private SearchController searchCon= new SearchController(allSchools);
+	
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@Test
@@ -18,7 +26,17 @@ public class SearchControllerTest {
 
 	@Test
 	public void testGetQuizQuestions() {
-		fail("Not yet implemented");
+		String expResult = "How would you describe the location of your ideal college?" + 
+				"Which characteristics are most important to you when looking for a college" + 
+				"I prefer large lectures where I can observe and be anonymous, rather than small discussions with participation and my professors know my name " +
+		"Do you know what you would like to study?";
+		String result = "";
+		String[] questions = searchCon.getQuizQuestions();
+		for(int i=0; i<questions.length; i++)
+		{
+			result += questions[i];
+		}
+		assertEquals(result, expResult);
 	}
 
 	@Test
